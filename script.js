672882027c1daf2ea27c0c02585d0d75adda2058
user <Thealexsuper2097@gmail.com>
2018-10-02 19:17:57 +0300

    update a button functions

--- script.js
+++ script.js
@@ -30,6 +30,7 @@ let scoreUser = 0;
 let scoreBot = 0;
 
 
+//select yuor move
 for (let i = 0; i < icons.length; i++) {
 	icons[i].addEventListener('click', click);
 	function click() {
@@ -55,82 +56,92 @@ for (let i = 0; i < icons.length; i++) {
 }
 
 button.addEventListener('click', clickOnButton);
-function clickOnButton(){
-	/*rand = Math.floor(Math.random() * 3) + 1; // random number to select the move*/
-	enemyChoose.appendChild(secondIco); // adding img to div
-
-	//show the timer
-	let numberOfSecond = 4;
-	setTimeout(count, 1000);
-	function count() {
-		let rand = Math.floor(Math.random() * 3) + 1; // random number to select the move
-	    numberOfSecond--;
-	    //algoritm of the game
-	    if (numberOfSecond > 0) {
-	      setTimeout(count, 1000);
-	    } 
-	    if (numberOfSecond === 0) {
-	    	numberOfSecond = 4;
-	    	count();//show again timer after 3sec over & over again before user's click "Stop game"
-
-	    	//bot choose
-	    	switch (rand) {
-			case 1:
-				secondIco.src = `ico-1.png`;
-				moveBot = 'paper';
-				break;
-			case 2:
-				secondIco.src = `ico-2.png`;
-				moveBot = 'rock';
-				break;
-			case 3:
-				secondIco.src = `ico-3.png`;
-				moveBot = 'scissors';
-				break;
-		}
-
-	    	/*buttonStop.onclick = function () {
-				clearTimeout(count);
-			}*/
-	    }
-	    timer.innerHTML = numberOfSecond;
-	}
-
-/*buttonStop.onclick = function () {
-		console.log('clicked!');
-		clearInterval(autoRun);
-	}*/
-	let timeDelay = numberOfSecond * 1000;
-	let delay = (() => {
-		//algoritm of the game
-		if (moveBot === moveUser) {
-			console.log('draw');
-		}
-		else if (moveUser === 'paper' && moveBot === 'rock') {
-			console.log('user win!');
-			scoreUser += 1; 
-			selectScoreUser.innerHTML = scoreUser;
-		}
-		else if (moveUser === 'rock' && moveBot === 'scissors') {
-			console.log('user win!');	
-			scoreUser += 1;
-			selectScoreUser.innerHTML = scoreUser;
-		}
-		else if (moveUser === 'scissors' && moveBot === 'paper') {
-			console.log('user win!');
-			scoreUser += 1;
-			selectScoreUser.innerHTML = scoreUser;
-		} else if (moveUser === '' | moveBot === '') {
-			scoreBot = scoreBot;
-			scoreUser = scoreUser;
+	let active = 0;
+	button.onclick = function() {
+		if (active === 0) {
+			active = 1;
+			button.textContent = 'Stop';
+			button.style.backgroundColor = '#f45c42';
 		} else {
-			console.log('you lose!');
-			console.log(`moveUser: ${moveUser} \n moveBot: ${moveBot}`);
-			scoreBot += 1;
-			selectScoreBot.innerHTML = scoreBot;
+			active = 0;
+			button.textContent = 'Let\'s start';
+			button.style.backgroundColor = '#46e246';
+
 		}
-	});
-	setInterval(delay, timeDelay);//run the game over and over again before user's scick on "stop game" 
+	}
+function clickOnButton(){
+	let interval = null;
+	let timeOut = null;
+	if (active === 0) {
+		enemyChoose.appendChild(secondIco); // adding img to div
+
+		//show the timer
+		let numberOfSecond = 4;
+		timeOut = setTimeout(count, 1000);
+		function count() {
+			let rand = Math.floor(Math.random() * 3) + 1; // random number to select the move
+		    numberOfSecond--;
+		    if (numberOfSecond > 0) {
+		      setTimeout(count, 1000);
+		    } 
+		    if (numberOfSecond === 0) {
+		    	numberOfSecond = 4;
+		    	count();//show again timer after 3sec over & over again before user's click "Stop game"
+
+		    	//computer choose
+		    	switch (rand) {
+				case 1:
+					secondIco.src = `ico-1.png`;
+					moveBot = 'paper';
+					break;
+				case 2:
+					secondIco.src = `ico-2.png`;
+					moveBot = 'rock';
+					break;
+				case 3:
+					secondIco.src = `ico-3.png`;
+					moveBot = 'scissors';
+					break;
+				}
+		    }
+		    timer.innerHTML = numberOfSecond;
+		}
+		let timeDelay = numberOfSecond * 1000;
+		let delay = (() => {
+			//algoritm of the game
+			if (moveUser === '' | moveBot === '') {
+				
+			}
+		    else if (moveBot === moveUser) {
+				console.log('draw');
+			}
+			else if (moveUser === 'paper' && moveBot === 'rock') {
+				console.log('user win!');
+				scoreUser += 1; 
+				selectScoreUser.innerHTML = scoreUser;
+			}
+			else if (moveUser === 'rock' && moveBot === 'scissors') {
+				console.log('user win!');	
+				scoreUser += 1;
+				selectScoreUser.innerHTML = scoreUser;
+			}
+			else if (moveUser === 'scissors' && moveBot === 'paper') {
+				console.log('user win!');
+				scoreUser += 1;
+				selectScoreUser.innerHTML = scoreUser;
+			} else {
+				console.log('you lose!');
+				console.log(`moveUser: ${moveUser} \n moveBot: ${moveBot}`);
+				scoreBot += 1;
+				selectScoreBot.innerHTML = scoreBot;
+			}
+		});
+		interval = setInterval(delay, timeDelay);//run the game over and over again before user's scick on "stop game" 
+	} else {
+		clearInterval(interval);
+		clearTimeout(timeOut);
+		return;
+	}
 }
 
 
